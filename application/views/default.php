1e56ed28bc2199fab3d3e96b77217c9cb9da121d
kadek bagus <kadekdarma90@gmail.com>
2017-08-19 13:00:14 +0800

    edit default.php

--- application/views/default.php
+++ application/views/default.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Default Page</title>
     <link href="<?php echo asset_url(); ?>css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo asset_url(); ?>css/font-awesome.css" rel="stylesheet">
     <link href="<?php echo asset_url(); ?>css/style.css" rel="stylesheet">
     <style>
     	body {
@@ -13,16 +14,48 @@
 </head>
 <body>
 
-<div class="container">
-<div class="row">
-	<div class="col-lg-6">
-	<p><?= $text1; ?></p>
-	</div>
-	<div class="col-lg-6">
-	<p><?= $text2; ?></p>
-	</div>
-</div>
-</div>
+<header>
+    <div class="container">
+        <div id="branding">
+            <h1><i class="fa fa-rocket"></i> <span class="highlight">ABC</span> Web design</h1>
+        </div>
+        <nav>
+            <ul>
+             <li><a href="#">Home</a></li>       
+             <li><a href="#">About</a></li>       
+             <li><a href="#">Services</a></li>             
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<section id="showcase">
+    <div class="container">
+        <h1>Haloo apa kabar?</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley </p>
+    </div>
+</section>
+
+<section id="newsletter">
+    <div class="container">
+        <h1></h1>
+        <form>
+            <input type="email" placeholder="enter email address">
+            <button type="submit" class="button1">Subscribe</button>
+        </form>
+    </div>
+</section>
+
+<section id="boxes">
+    <div class="container">
+        <div class="box">
+        </div>
+    </div>
+</section>
+
+<footer>
+    
+</footer>
 
 <script src="<?php echo asset_url(); ?>js/jquery-3.2.1.min.js"></script>    
 <script src="<?php echo asset_url(); ?>js/bootstrap.min.js"></script>   
